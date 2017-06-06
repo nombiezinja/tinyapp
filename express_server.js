cf674031da833ed04e598b66a7e781e9891c6586
Ti Zhang <tizhang8835@gmail.com>
2017-06-06 23:52:47 +0000

    Adds random string generation function using  base 36

--- express_server.js
+++ express_server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.set("view engine", "ejs");
 
@@ -23,12 +26,32 @@ app.get('/hello', (req,res) => {
 
 app.get('/urls', (req, res) => {
   let templateVars = { urls: urlDatabase };
-  console.log(typeof templateVars);
-  console.log(templateVars);
   res.render('urls_index', templateVars);
+});
 
-})
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id };
+  res.render("urls_show", templateVars);
+});
+
+app.get("/urls/make_new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+
+function randomString(length) {
+    return Math.round((Math.pow(36, length + 1) - Math.random() * Math.pow(36, length))).toString(36).slice(1);
+}
